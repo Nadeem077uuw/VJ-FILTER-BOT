edfaa5679f1b9e4d285c80c3a1c34010b7d1fe19
Nadeem077uuw <dknadeem582@gmail.com>
2024-07-27 17:46:02 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -645,10 +645,10 @@ async def send_all(bot, userid, files, ident, chat_id, user_name, query):
                         reply_markup=InlineKeyboardMarkup(
                             [
                                 [
-                                InlineKeyboardButton('Sᴜᴘᴘᴏʀᴛ Gʀᴏᴜᴘ', url=GRP_LNK),
-                                InlineKeyboardButton('Uᴘᴅᴀᴛᴇs Cʜᴀɴɴᴇʟ', url=CHNL_LNK)
+                                InlineKeyboardButton('Movie Group', url=GRP_LNK),
+                                InlineKeyboardButton('Anime Channel', url=CHNL_LNK)
                             ],[
-                                InlineKeyboardButton("Bᴏᴛ Oᴡɴᴇʀ", url="t.me/creatorrio")
+                                InlineKeyboardButton("Bᴏᴛ Oᴡɴᴇʀ", url="t.me/RoronoaZo0ro")
                                 ]
                             ]
                         )
